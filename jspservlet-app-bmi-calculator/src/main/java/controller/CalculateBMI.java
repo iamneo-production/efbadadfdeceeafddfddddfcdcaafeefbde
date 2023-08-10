0a29ed7ead1a804617e35a51e549f16fc9e84484
Iamneo <techadmin@iamneo.ai>
2023-08-10 09:26:02 +0000

    Backup Commit

--- jspservlet-app-bmi-calculator/src/main/java/controller/CalculateBMI.java
+++ jspservlet-app-bmi-calculator/src/main/java/controller/CalculateBMI.java
@@ -1,37 +1,36 @@
 package controller;
+import java.text.DecimalFormat;
+public class CalculateBMI {
 
-public class CalculateBMI
- {
-
-
-		public static double calculate(double height, double weight)
-		 {
-		  double bmi = weight / (height * height);
-		  return bmi;
-		}
-	  
-		public static String description(double bmi)
-		 {
-		  if (bmi < 18.5) 
-		  {
-			return "Underweight";
-		  } 
-		  else if (bmi >= 18.5 && bmi < 24.9)
-		 {
-			return "Normal";
-		  } 
-		  else if (bmi >= 25 && bmi < 29.9)
-		   {
-			return "Overweight";
-		  }
-		   else if (bmi >= 30 && bmi < 34.9) 
-		   {
-			return "Obese";
-		  }
-		   else
-		    {
-			return "Extremely Obese";
-		  }
-		}
-	  
+	public static double calculate(double height, double weight)
+	{
+	DecimalFormat df=new DecimalFormat("0.00");
+	 double bmi = weight / (height * height);
+	 bmi=Double.parseDouble(df.format(bmi*10000));
+	 return bmi;
+   }
+ 
+   public static String description(double bmi)
+	{
+	 if (bmi < 18.5) 
+	 {
+	   return "Under Weight";
+	 } 
+	 else if (bmi >= 18.5 && bmi < 24.9)
+	{
+	   return "Normal";
+	 } 
+	 else if (bmi >= 25 && bmi < 29.9)
+	  {
+	   return "Over Weight";
+	 }
+	  else if (bmi >= 30 && bmi < 34.9) 
+	  {
+	   return "Obese";
+	 }
+	  else
+	   {
+	   return "Extremely Obese";
+	 }
+   }
 }
